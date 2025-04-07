398b00f4be8a08bc998aa70df26a5e90d7b4f442
Your Name <ayatabdelrhman514@gmail.com>
2025-04-07 05:06:21 +0200

    cors

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import path from "path"
 import express from "express"
 import { connectDb } from "./db/connection.js"
 import { initapp } from "./src/initApp.js"
+import cors from 'cors';
 import dotenv from "dotenv"
 const app=express()
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true 
+}));
 dotenv.config({path:path.resolve('./config/.env')})
 connectDb()
 initapp(app,express)
