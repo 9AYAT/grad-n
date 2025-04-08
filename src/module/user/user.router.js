c8becd09a9393d2b33b83143c833d2c8c4f59f14
Your Name <ayatabdelrhman514@gmail.com>
2025-04-08 18:19:43 +0200

    va

--- src/module/user/user.router.js
+++ src/module/user/user.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { isvalid } from "../../middleware/validtion.js";
 import { asynchandler } from "../../middleware/asynchandler.js";
-import { forgetpassVal, loginVal, resetVal, signupVal } from "./user.validation.js";
+import { forgetpassVal, loginVal,resetVal,signupVal } from "./user.validation.js";
 import { forgetPassword, login, resetPassword, signup, verifyAccount } from "./user.controller.js";
 const userRouter=Router()
 userRouter.post('/signups',isvalid(signupVal),asynchandler(signup))
