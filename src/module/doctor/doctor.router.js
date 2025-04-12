175c5c2341c00f1f23884b76b5cf21467175b117
Your Name <ayatabdelrhman514@gmail.com>
2025-04-13 01:01:24 +0200

    async

--- src/module/doctor/doctor.router.js
+++ src/module/doctor/doctor.router.js
@@ -3,8 +3,8 @@ import { isAuthenticated } from "../../middleware/authentication.js";
 import { isAuthorized } from "../../middleware/authrization.js";
 import { roles } from "../../utils/constant/enum.js";
 import { asynchandler } from "../../middleware/asynchandler.js";
-
+import { AddDoctor } from "./doctor.controller.js";
 const doctorRouter=Router()
 //add doctor
-doctorRouter.post('/add',isAuthenticated(),isAuthorized(roles.USER),asynchandler())
+doctorRouter.post('/add',isAuthenticated(),isAuthorized(roles.USER),asynchandler(AddDoctor))
 export default doctorRouter
