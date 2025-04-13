e9541281aabcaa05d0d9c66187e4fc4e8a3b3243
Your Name <ayatabdelrhman514@gmail.com>
2025-04-14 01:39:10 +0200

    speciality

--- src/module/doctor/doctor.controller.js
+++ src/module/doctor/doctor.controller.js
@@ -1,12 +1,13 @@
 import { Doctor } from "../../../db/index.js"
 export const AddDoctor=async(req,res,next)=>{
-    const{name,phone,specialist,location,price}=req.body
+    const{name,phone,specialist,location,price,specialty}=req.body
     const doctor=new Doctor({
         name,
         phone,
         specialist,
         location,
-        price
+        price,
+        specialty
     })
     await doctor.save()
     return res.status(201).json({messsage:'doctor added successfully',success:true})
