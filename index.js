93b818a8c7857cd16bad7771643e00b12ac9994c
Your Name <ayatabdelrhman514@gmail.com>
2025-05-09 14:49:44 +0300

    jk

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ import cors from 'cors';
 import dotenv from "dotenv"
 import bodyParser from "body-parser";
 const app=express()
-//app.use(cors({
- // origin: 'http://localhost:4200',
-  // credentials: true 
-//}));
 app.use(cors({
-  //origin: "*", // للسماح بالوصول من أي مكان (Flutter, web)
-   origin: ['http://localhost:4200'],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: 'http://localhost:4200',
+   credentials: true 
 }));
+//app.use(cors({
+  //origin: "*", // للسماح بالوصول من أي مكان (Flutter, web)
+  // origin: ['http://localhost:4200'],
+  //methods: ["GET", "POST", "PUT", "DELETE"],
+ // allowedHeaders: ["Content-Type", "Authorization"],
+//}));
 
 app.use(bodyParser.json());
 dotenv.config({path:path.resolve('./config/.env')})
